82842d9ab16b86daf6f05a1d8497749c0b864eda
Den Brause <neokrasav4ik@gmail.com>
2026-09-07 09:58:45 +0300

    Add files via upload

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -68,8 +68,8 @@ android {
         applicationId = "ru.neokrasav4ik.lepramobile"
         minSdk = 26
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.0.6"
+        versionCode = 12
+        versionName = "1.0.7"
     }
 
     signingConfigs {
